1a538c5ac9be1a53cfd41e20d1d231f6c0a8687d
Dan O'Mahony <danomahony4@gmail.com>
2016-05-02 10:44:35 +0100

    Added lines 1 to 42
    Initial code.
    Pushes each values charcode into an array.
    Visible what charCode is missing

---
+++ 08 Missing Letters.js
@@ -0,0 +1,42 @@
+
+/**********************************
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+String.charCodeAt()
+String.fromCharCode()
+**********************************/
+function fearNotLetter(str) {
+  
+  //Set up a holder array to hold charCodes
+  var holderArray = [];
+  
+  //Find the charCode of each of the elements
+  for(var i = 0; i < str.length; i++) {
+    //Push each value into holderArray
+    holderArray.push(str.charCodeAt(i));
+   }//end for loop
+  
+  
+    //Test console.log to test if the index is one value larger than the previous
+    console.log(holderArray[8] == holderArray[7] + 1);
+  
+  //Iterate (?) through the charCodes
+    
+      //Each charCode (starting at index 1?) should be 1 greater than the previous
+      //If encounter a gap, return the missing charCode
+
+      //Turn the charCode into its respective letter
+      //Return the missing letter
+      //if there is nothing missing return undefined
+      
+
+  return holderArray;
+}
+
+fearNotLetter("abcdefghjklmno");
