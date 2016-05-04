4ef9e5909d8698b8192034372c25d67a75d53d69
Dan O'Mahony <danomahony4@gmail.com>
2016-05-04 11:05:54 +0100

    Tidied up comments

--- 08 Missing Letters.js
+++ 08 Missing Letters.js
@@ -1,13 +1,9 @@
 
 /**********************************
 Find the missing letter in the passed letter range and return it.
-
 If all letters are present in the range, return undefined.
-
 Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
 Here are some helpful links:
-
 String.charCodeAt()
 String.fromCharCode()
 **********************************/
@@ -31,8 +27,6 @@ function fearNotLetter(str) {
         }//end if
       }//end for
 
-      
-
-}
+}//end fearNotLetter
 
 fearNotLetter("abcdefghjklmno");
