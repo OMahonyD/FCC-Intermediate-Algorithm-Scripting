806e3a06f389f720d9fa134fb197cdc5aef2128a
Dan O'Mahony <danomahony4@gmail.com>
2016-05-07 14:48:03 +0100

    Added lines 1 to 19
    Regular expression currently successful tests 1, 3, 4. Need to find regex to find uppercase letter immediately after lowercase and add to var re.

---
+++ 10 Spinal Tap Case.js
@@ -0,0 +1,18 @@
+/*************************************************************
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+RegExp
+String.replace()
+*************************************************************/
+
+function spinalCase(str) {
+  //need to put a hyphen where there is a space or before an uppercase letter
+  var re=/[\_\W]/g;//[\_\W] matches tests 1, 3 and 4. Need to find regex to find upprcase letter immediately after lowercase letter
+  var newStr = str.replace(re, "-").toLowerCase();
+  return newStr;
+}
+spinalCase('AllThe-small Things');
