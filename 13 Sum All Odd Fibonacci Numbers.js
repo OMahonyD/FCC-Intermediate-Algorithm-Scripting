fe4c1282dafc14a7991cc90bb1b0f5257134bdac
Dan O'Mahony <danomahony4@gmail.com>
2016-05-10 08:22:12 +0100

    Added lines 28 to 35 to finalise algorithm

--- 13 Sum All Odd Fibonacci Numbers.js
+++ 13 Sum All Odd Fibonacci Numbers.js
@@ -10,19 +10,31 @@ Here are some helpful links:
 
 function sumFibs(num) {
   //Array of all fibonacci numbers
-  var fib = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
+  var fib = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
              17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578,
              5702887];
   //Array to hold all odd fibonacci numbers
   var oddFib = [];
+  //Array to send odd fibonacci numbers that pass the second test to
+  var result = 0;
   
-  //Iterate through fib array to find odd numbers
+  //Iterate through fib array to find odd fibonacci numbers
   for(var i=0; i < fib.length; i++) {
     if(fib[i] % 2 !== 0) {
       oddFib.push(fib[i]);
     }//end if
   }//end for
-  return oddFib;
+  
+  //Iterate through the oddFib array to find numbers that pass the if test
+  for(var j = 0; j < oddFib.length; j++) {
+    if(oddFib[j] <= num) {
+      result += oddFib[j];      
+    }//end if
+  }//end for
+  
+  return result;
+ 
 }
 
 sumFibs(4);
+
