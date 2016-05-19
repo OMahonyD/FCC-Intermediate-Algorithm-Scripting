e7c8b0aa2f4a5a5f441e9f84b2eb19556e760a69
Dan O'Mahony <danomahony4@gmail.com>
2016-05-19 08:35:27 +0100

    Added lines 1 to 18

---
+++ 18 Steamroller.js
@@ -0,0 +1,18 @@
+/*********************************************
+Flatten a nested array. You must account for varying levels of nesting.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.isArray()
+*********************************************/
+
+function steamrollArray(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+  }, []);
+}
+
+// test here
+steamrollArray([1, [2], [3, [[4]]]]);
