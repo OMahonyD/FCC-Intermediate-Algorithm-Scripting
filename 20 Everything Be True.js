20b253fb8b0b85c996f53d828f293975b67fb13f
Dan O'Mahony <danomahony4@gmail.com>
2016-05-21 09:31:47 +0100

    Added lines 1 to 15

---
+++ 20 Everything Be True.js
@@ -0,0 +1,15 @@
+/**********************************************************
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+**********************************************************/
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every(function(i){
+    return i.hasOwnProperty(pre) && Boolean(i[pre]);
+  });
+}
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
